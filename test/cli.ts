67fcfb916f224bc5276639109d21e79af8eaf87c
Nat Burns <nbaccount@burnskids.com>
2018-06-28 11:55:08 +0000

    Prevent CLI tests from exiting prematurely.

--- test/cli.ts
+++ test/cli.ts
@@ -1,10 +1,34 @@
 import run from 'test-cli';
 import cli from '../src/cli';
 
-const flowToDts = run.bind(null, cli);
+interface Process {
+	stdout?: any;
+	exit?: any;
+}
+
+let origProcess: Process = {};
+
+const assignProcess = (src: Process, dest: Process) => {
+	dest.stdout = src.stdout;
+	dest.exit = src.exit;
+}
+
+const flowToDts = run.bind(null, cli, (mockProcess: Process) => {
+	assignProcess(process, origProcess);
+	assignProcess(mockProcess, process);
+});
+
+const cleanup = () => {
+	assignProcess(origProcess, process);
+};
 
 describe('command line', () => {
 	it('shows help', () => {
-		flowToDts('--help', () => {});
+		try {
+			flowToDts('--help', () => {});
+		}
+		catch (e) {
+			// Expected for help.
+		}
 	});
 });
